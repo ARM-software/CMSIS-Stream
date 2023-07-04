add25a135d1d2d401089d1eb2f9d82e2f844389a
Christophe Favergeon <Christophe.Favergeon@arm.com>
2023-07-04 08:21:09 +0200

    Upgraded eventrecorder example to CMSIS build tools 2.0

--- Examples/eventrecorder/GetEvent_cs300.bat
+++ Examples/eventrecorder/GetEvent_cs300.bat
@@ -1,5 +1,5 @@
 eventlist.exe ^
- -a out\eventrecorder\VHT-Corstone-300\CommandLine\CommandLine+VHT-Corstone-300.axf ^
+ -a out\eventrecorder\VHT-Corstone-300\CommandLine\eventrecorder.axf ^
  -I ..\..\cg.scvd ^
  -I C:\Keil_v5\ARM\PACK\Keil\ARM_Compiler\1.7.2\EventRecorder.scvd ^
  -I C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.9.0\ARM.CMSIS.pdsc ^
