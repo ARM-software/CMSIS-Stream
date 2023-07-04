9f9110b82031c9a8459c40562e1666b958d5b9c2
Christophe Favergeon <Christophe.Favergeon@arm.com>
2023-07-04 08:32:41 +0200

    Upgraded tests to use CMSIS build tools 2.0

--- Tests/run_fifobench_async.bat
+++ Tests/run_fifobench_async.bat
@@ -1,4 +1,4 @@
 C:\Keil_v5\ARM\VHT\VHT_Corstone_SSE-300_Ethos-U55.exe ^
   -f ARMCM55_FP_MVE_config.txt ^
   -C cpu0.semihosting-enable=1 ^
-  -a cpu0="out\fifobench_async\VHT-Corstone-300\CommandLine\CommandLine+VHT-Corstone-300.axf" 
+  -a cpu0="out\fifobench_async\VHT-Corstone-300\CommandLine\fifobench_async.axf" 
