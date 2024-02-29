e5e05790aeafb1c6441690fca4155674747e88af
Christophe Favergeon <Christophe.Favergeon@arm.com>
2024-02-29 10:24:56 +0100

    runtime_mode example: This example is no more part of the standard build of examples So references to flat buffer path have been removed from the scripts.

--- Examples/build/create.bat
+++ Examples/build/create.bat
@@ -1,5 +1,4 @@
 cmake -DHOST=YES ^
    -DDOT="path to dot.EXE" ^
    -DCMSISDSP="path to cmsis dsp" ^
-   -DFLAT="path to flatbuffer includes for runtime mode of CMSIS stream" ^
    -G "Unix Makefiles" ..
