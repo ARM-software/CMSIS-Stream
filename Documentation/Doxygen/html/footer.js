268dd8234a6452f64eaa63f20a0bffb23e7a1ba3
Christophe Favergeon <Christophe.Favergeon@arm.com>
2026-05-19 10:17:07 +0200

    Remove doxygen warnings when building the doc

--- Documentation/Doxygen/html/footer.js
+++ Documentation/Doxygen/html/footer.js
@@ -1,7 +1,7 @@
 function writeHeader() {
-    document.write('Version v3.2.0-18-ga34eca3-dirty');
+    document.write('Version v3.2.0-21-gd7ffd6c-dirty');
 };
 
 function writeFooter()  {
-    document.write('Generated on Tue May 19 2026 07:25:02 for CMSIS-Stream v3.2.0-18-ga34eca3-dirty. Copyright &copy; 2026 Arm Limited (or its affiliates). All rights reserved.');
+    document.write('Generated on Tue May 19 2026 08:15:06 for CMSIS-Stream v3.2.0-21-gd7ffd6c-dirty. Copyright &copy; 2026 Arm Limited (or its affiliates). All rights reserved.');
 };
